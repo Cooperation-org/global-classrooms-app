41908ccb66511a4a76e63e94960b477aca4b96c1
romeoscript <romeobourne211@gmail.com>
2025-06-23 19:19:48 +0100

    getting projects

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -5,7 +5,7 @@ export default function DashboardHome() {
     <div className="flex flex-col items-center justify-center w-full h-full">
       <h1 className="text-2xl font-bold text-center mb-4">Welcome to Global Classrooms!</h1>
       <h2 className="text-xl font-semibold text-center mb-2">Let&apos;s get your school ready to make an impact.</h2>
-      <p className="text-center text-gray-600 max-w-xl mb-8">
+      <p className="text-center text-gray-600 max-w-3xl mb-8">
         You&apos;re now set to build your digital classroom. Here are your next steps: Register your school, Add teachers, Add students.<br />
         Don&apos;t worry - we&apos;ll guide you through each step.
       </p>
