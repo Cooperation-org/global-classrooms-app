55a2c2d5132bb64955aa5699d4e437e26ecb920f
Agnes Koinange <koinangeagness@gmail.com>
2025-07-31 15:29:18 +0300

    Add missing navigation pages: why-we-exist and contact

---
+++ app/why-we-exist/page.tsx
@@ -0,0 +1,9 @@
+import WhyWeExistSection from '../components/sections/WhyWeExistSection';
+
+export default function WhyWeExistPage() {
+  return (
+    <div className="min-h-screen">
+      <WhyWeExistSection />
+    </div>
+  );
+}
