9cbe8f133d00e19a1bccef86d45863b2273a26b6
Agnes Koinange <koinangeagness@gmail.com>
2025-07-31 13:49:41 +0300

    Remove mobile sidebar overlay to keep page visible

--- app/components/layout/Sidebar.tsx
+++ app/components/layout/Sidebar.tsx
@@ -147,16 +147,9 @@ const Sidebar: React.FC<SidebarProps> = ({ open = false, onClose }) => {
 
   return (
     <>
-      {/* Mobile overlay */}
-      {open && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-40 md:hidden"
-          onClick={onClose}
-        />
-      )}
       
       {/* Sidebar */}
-      <aside className={`fixed top-0 left-0 h-screen w-64 bg-white border-r border-gray-100 flex flex-col px-6 py-8 z-30 transition-transform duration-300 ease-in-out ${
+      <aside className={`fixed top-0 left-0 h-screen w-64 bg-white border-r border-gray-100 flex flex-col px-6 py-8 z-50 transition-transform duration-300 ease-in-out ${
         open ? 'translate-x-0' : '-translate-x-full'
       } md:translate-x-0`}>
         {/* Close button for mobile */}
